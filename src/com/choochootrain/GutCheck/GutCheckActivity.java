2078623a1e1d8ba5aa80631e7199364d49613020
Hurshal Patel <hpatel516@gmail.com>
2011-12-24 09:02:07 -0800

    added colored buttons

--- src/com/choochootrain/GutCheck/GutCheckActivity.java
+++ src/com/choochootrain/GutCheck/GutCheckActivity.java
@@ -1,21 +1,46 @@
 package com.choochootrain.GutCheck;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class GutCheckActivity extends Activity {
+	private Button yesButton;
+	private Button noButton;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        yesButton = (Button)findViewById(R.id.button_yes);
+        yesButton.getBackground().setColorFilter(0xFF00CC00, PorterDuff.Mode.MULTIPLY);
+        yesButton.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		//record this as a productive hour
+        		recordAction(true);
+        	}
+        });
+        
+        noButton = (Button)findViewById(R.id.button_no);
+        noButton.getBackground().setColorFilter(0xFFCC0000, PorterDuff.Mode.MULTIPLY);
+        noButton.setOnClickListener(new OnClickListener() {
+        	@Override
+        	public void onClick(View v) {
+        		//record this as an unproductive hour
+        		recordAction(false);
+        	}
+        });
         
         //start background service
         startService(new Intent(GutCheckService.class.getName()));
     }
+    
+	protected void recordAction(boolean isProductive) {
+		
+	}
 }
